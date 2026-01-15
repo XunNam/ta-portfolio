254ce3f561aa8020439a0952e7448bc59a661736
Bùi Thế Anh <bui.theanh@outlook.com>
2026-01-15 19:20:06 +0700

    Tạo thanh NavBar
    Tạo thanh NavBar đơn giản và tối ưu cho giao diện điện thoại (responsive).

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import './styles.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+// Phông chữ
+import { Roboto_Mono, Inter } from 'next/font/google'
+
+//Components
+import NavigationBar from '@/components/Navigation Bar/Navigation Desktop'
+
+export const metadata: Metadata = {
+  description: 'Một dự án Portfolio được xây dựng trên nền tảng NextJS.',
+  title: 'Thế Anh | Portfolio',
+  icons: {
+    icon: '/icon/vercel.ico',
+  },
 }
+
+// Khai báo phông chữ
+export const roboto_mono = Roboto_Mono({
+  variable: '--font-roboto-mono',
+  subsets: ['latin'],
+})
+
+export const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="vi" className={`${roboto_mono.variable} ${inter.variable} antialiased`}>
       <body>
+        <nav>
+          <NavigationBar />
+        </nav>
         <main>{children}</main>
       </body>
     </html>
